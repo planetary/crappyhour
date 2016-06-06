7eb6f6bcb14a08d1bc78b9a37efd34d8fdaa2aa6
Sidlav <redhotfusion@Valdiss-MBP.lan>
2016-06-06 22:26:13 +0300

    Added and styled SVG clocks

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,14 +4,19 @@ module.exports = {};
 
 $( document ).ready(function() {
 
+  var numbr = 50;
 
-;
+  setInterval(function() {
+	function r(el, deg) {
+		el.setAttribute('transform', 'rotate('+ deg +' 50 50)')
+	}
+	var d = new Date()
+	r(hour, 30*(d.getHours()%12) + d.getMinutes()/2)
+  r(min, 6*d.getMinutes())
+  r(sec, 6*d.getSeconds())
+}, 1000)
+
 
 
 
 });
-
-
-$(window).load(function() {
-
-})
