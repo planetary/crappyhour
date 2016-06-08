3f24ad0926ebef2ba9ff6745392bd9b39931f0ac
Samantha Ingram <sam@rndpxl.com>
2016-06-08 16:36:36 -0400

    Refactor.. a little bit better.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,16 +4,26 @@ const moment = require('moment');
 require('moment-countdown');
 
 // Define the Start & End Date
-const eventStartDate = '2016-06-22 18:00:00 GMT-0400';
-const eventEndDate = '2016-06-22 20:00:00 GMT-0400';
+const eventStartDate = '2016-06-04 18:00:00 GMT-0400';
+const eventEndDate = '2016-06-04 20:00:00 GMT-0400';
+
+const setClockNumbers = (date) => {
+    const days = moment().countdown(date).days;
+    const hours = moment().countdown(date).hours;
+    const minutes = moment().countdown(date).minutes;
+    const seconds = moment().countdown(date).seconds;
+    $('.countdown-text.days').text(`${days} Days`);
+    $('.countdown-text.hours').text(`${hours} Hours`);
+    $('.countdown-text.minutes').text(`${minutes} Minutes`);
+    $('.countdown-text.seconds').text(`${seconds} Seconds`);
+};
 
 setInterval(function() {
-
     // Animate the Clock Hands
     if(!moment().isAfter(eventEndDate)) {
         const r = (el, deg) => {
             el.setAttribute('transform', `rotate(${deg} 50 50)`);
-        }
+        };
         const d = new Date();
         r(hour, 30 * (d.getHours() % 12) + d.getMinutes() / 2);
         r(min, 6 * d.getMinutes());
@@ -21,36 +31,19 @@ setInterval(function() {
     }
 
     // Count down to event start date
-    if(moment().isBefore(eventStartDate)) {
-        const days = moment().countdown(eventStartDate).days;
-        const hours = moment().countdown(eventStartDate).hours;
-        const minutes = moment().countdown(eventStartDate).minutes;
-        const seconds = moment().countdown(eventStartDate).seconds;
-        $('.countdown-text.days').text(`${days} Days`);
-        $('.countdown-text.hours').text(`${hours} Hours`);
-        $('.countdown-text.minutes').text(`${minutes} Minutes`);
-        $('.countdown-text.seconds').text(`${seconds} Seconds`);
-    }
+    if(moment().isBefore(eventStartDate))
+        setClockNumbers(eventStartDate);
 
     // Countdown to event end date
-    if(moment().isSameOrAfter(eventStartDate) && moment().isSameOrBefore(eventEndDate)) {
-        const days = moment().countdown(eventEndDate).days;
-        const hours = moment().countdown(eventEndDate).hours;
-        const minutes = moment().countdown(eventEndDate).minutes;
-        const seconds = moment().countdown(eventEndDate).seconds;
-        $('.countdown-text.days').text(`${days} Days`);
-        $('.countdown-text.hours').text(`${hours} Hours`);
-        $('.countdown-text.minutes').text(`${minutes} Minutes`);
-        $('.countdown-text.seconds').text(`${seconds} Seconds`);
-    }
+    if(moment().isSameOrAfter(eventStartDate) && moment().isSameOrBefore(eventEndDate))
+        setClockNumbers(eventEndDate);
 
     if(moment().isAfter(eventEndDate)) {
-        $('.countdown-text.days').text(`88:88 Days`);
-        $('.countdown-text.hours').text(`88:88 Hours`);
-        $('.countdown-text.minutes').text(`88:88 Minutes`);
-        $('.countdown-text.seconds').text(`88:88 Seconds`);
+        $('.countdown-text.days').text(`88:88`);
+        $('.countdown-text.hours').text(`88:88`);
+        $('.countdown-text.minutes').text(`88:88`);
+        $('.countdown-text.seconds').text(`88:88`);
     }
-
 }, 1000);
 
 // Event Details
@@ -67,7 +60,8 @@ if(moment().isBefore(eventStartDate)) {
 // If Event is Happening
 if(moment().isSameOrAfter(eventStartDate) && moment().isSameOrBefore(eventEndDate)) {
     $('.event-copy').append(
-        `#crappyhour is happening right now until 8pm, at ${eventLocation}! If you hurry, you might be able to make it. Sign up below for a reminder about next month's event.`
+        `#crappyhour is happening right now until 8pm, at ${eventLocation}! If you hurry,
+        you might be able to make it. Sign up below for a reminder about next month's event.`
     );
 }
 
