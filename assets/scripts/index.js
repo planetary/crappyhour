4e2936fa9bb546061cf664a9766f4947898df47a
Sidlav <redhotfusion@Valdiss-MBP.lan>
2016-06-08 16:42:14 +0300

    Added countdown script + minor styling and SVG logo implementation

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,19 +1,32 @@
 module.exports = {};
 
+var moment = require('moment');
+require('moment-countdown');
 
+
+
+
+$("#countdown").text(moment().countdown("2016-07-09").toString());
 
 $( document ).ready(function() {
+
+  moment().format();
+
+
+
 
   var numbr = 50;
 
   setInterval(function() {
 	function r(el, deg) {
-		el.setAttribute('transform', 'rotate('+ deg +' 50 50)')
+	    el.setAttribute('transform', 'rotate('+ deg +' 50 50)')
 	}
 	var d = new Date()
 	r(hour, 30*(d.getHours()%12) + d.getMinutes()/2)
   r(min, 6*d.getMinutes())
   r(sec, 6*d.getSeconds())
+  $("#countdown").text(moment().countdown("2016-07-21").toString());
+
 }, 1000)
 
 
