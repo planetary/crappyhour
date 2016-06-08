d5dc17ab9c6ac26e6e29ea283ef9e4d86d1ef9f6
Samantha Ingram <sam@rndpxl.com>
2016-06-08 17:57:15 -0400

    Cleanup Variables

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,23 +4,26 @@ const moment = require('moment');
 require('moment-countdown');
 
 // Define the Start & End Date
-const eventStartDate = '2016-06-04 18:00:00 GMT-0400';
-const eventEndDate = '2016-06-04 20:00:00 GMT-0400';
+const eventStartDate = '2016-06-22 18:00:00 GMT-0400';
+const eventEndDate = '2016-06-22 20:00:00 GMT-0400';
 
+// Define when to switch content
+const beforeEvent = moment().isBefore(eventStartDate);
+const duringEvent = moment().isAfter(eventStartDate) && moment().isBefore(eventEndDate);
+const afterEvent = moment().isAfter(eventEndDate);
+
+// Set Clock Value
 const setClockNumbers = (date) => {
-    const days = moment().countdown(date).days;
-    const hours = moment().countdown(date).hours;
-    const minutes = moment().countdown(date).minutes;
-    const seconds = moment().countdown(date).seconds;
-    $('.countdown-text.days').text(`${days} Days`);
-    $('.countdown-text.hours').text(`${hours} Hours`);
-    $('.countdown-text.minutes').text(`${minutes} Minutes`);
-    $('.countdown-text.seconds').text(`${seconds} Seconds`);
+    const countdown = moment().countdown(date);
+    $('.countdown-text.days').text(`${countdown.days} Days`);
+    $('.countdown-text.hours').text(`${countdown.hours} Hours`);
+    $('.countdown-text.minutes').text(`${countdown.minutes} Minutes`);
+    $('.countdown-text.seconds').text(`${countdown.seconds} Seconds`);
 };
 
 setInterval(function() {
     // Animate the Clock Hands
-    if(!moment().isAfter(eventEndDate)) {
+    if(!afterEvent) {
         const r = (el, deg) => {
             el.setAttribute('transform', `rotate(${deg} 50 50)`);
         };
@@ -30,43 +33,36 @@ setInterval(function() {
         r(sec, 6 * d.getSeconds());
     }
 
-    // Count down to event start date
-    if(moment().isBefore(eventStartDate))
-        setClockNumbers(eventStartDate);
-
-    // Countdown to event end date
-    if(moment().isSameOrAfter(eventStartDate) && moment().isSameOrBefore(eventEndDate))
-        setClockNumbers(eventEndDate);
-
-    if(moment().isAfter(eventEndDate)) {
-        $('.countdown-text.days').text('88:88');
-        $('.countdown-text.hours').text('88:88');
-        $('.countdown-text.minutes').text('88:88');
-        $('.countdown-text.seconds').text('88:88');
+    if(beforeEvent) {
+        let clockVal = 'eventStartDate';
+    } else if(duringEvent) {
+        let clockVal = 'eventEndDate';
+    } else {
+        let clockVal = '88:88';
     }
+
+    setClockNumbers(clockVal);
+
 }, 1000);
 
-// Event Details
-const eventLocation = '<a href="">One Mile House</a>';
-const dateAndTime = 'June 22nd, 6-8pm';
-
-// If Event is Upcoming
-if(moment().isBefore(eventStartDate)) {
+// Content for before Event
+if(beforeEvent) {
     $('.event-copy').append(
-        `The next #crappyhour is ${dateAndTime} at ${eventLocation}.`
+        `The next #crappyhour is June 22nd, 6-8pm at <a href="">One Mile House</a>.`
     );
 }
 
-// If Event is Happening
-if(moment().isSameOrAfter(eventStartDate) && moment().isSameOrBefore(eventEndDate)) {
+// Content while event is happening
+if(duringEvent) {
     $('.event-copy').append(
-        `#crappyhour is happening right now until 8pm, at ${eventLocation}! If you hurry,
-        you might be able to make it. Sign up below for a reminder about next month's event.`
+        `#crappyhour is happening right now until 8pm, at <a href="">One Mile House</a>! If you
+        hurry, you might be able to make it. Sign up below for a reminder about next month's
+        event.`
     );
 }
 
-// If Event is Over
-if(moment().isAfter(eventEndDate)) {
+// Content when the event is over
+if(afterEvent) {
     $('.event-copy').append(
         `The next #crappyhour <a href="#">can't come soon enough</a>.`
     );
