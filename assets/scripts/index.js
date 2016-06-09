30de4f000395827e607feae2f9bbac84d3452558
Matthew Ström <matthew.h.strom@gmail.com>
2016-06-09 11:38:12 -0400

    clean up methods to check for valid dates

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,60 +10,61 @@ const eventEndDate = '2016-06-22 20:00:00 GMT-0400';
 // Define when to switch content
 const beforeEvent = moment().isBefore(eventStartDate);
 const duringEvent = moment().isAfter(eventStartDate) && moment().isBefore(eventEndDate);
-const afterEvent = moment().isAfter(eventEndDate);
 
 // Set Clock Value
 const setClockNumbers = (date) => {
+    if (moment(date).isValid()) {
+        const countdown = moment().countdown(date);
+        $('.countdown-text.days').text(`${countdown.days} Days`);
+        $('.countdown-text.hours').text(`${countdown.hours} Hours`);
+        $('.countdown-text.minutes').text(`${countdown.minutes} Minutes`);
+        $('.countdown-text.seconds').text(`${countdown.seconds} Seconds`);
+    } else {
+        $('.countdown-text').each(function() {
+            $(this).text(date);
+        });
+    }
+};
+
+const setClockHands = (date) => {
+    const r = (el, deg) => {
+        el.setAttribute('transform', `rotate(${deg} 50 50)`);
+    };
     const countdown = moment().countdown(date);
-    $('.countdown-text.days').text(`${countdown.days} Days`);
-    $('.countdown-text.hours').text(`${countdown.hours} Hours`);
-    $('.countdown-text.minutes').text(`${countdown.minutes} Minutes`);
-    $('.countdown-text.seconds').text(`${countdown.seconds} Seconds`);
+    if (moment(date).isValid()) {
+        r(document.getElementById('day'), 12 * countdown.days);
+        r(document.getElementById('hour'), 30 * (countdown.hours % 12) + countdown.minutes / 2);
+        r(document.getElementById('min'), 6 * countdown.minutes);
+        r(document.getElementById('sec'), 6 * countdown.seconds);
+    } else {
+
+        // spin the hands
+    }
+};
+
+const setCopy = (text) => {
+    if (!$('.event-copy').html())
+        $('.event-copy').append(text);
 };
 
 setInterval(function() {
-    // Animate the Clock Hands
-    if(!afterEvent) {
-        const r = (el, deg) => {
-            el.setAttribute('transform', `rotate(${deg} 50 50)`);
-        };
-        const d = new Date();
-        r(hour, 30 * (d.getHours() % 12) + d.getMinutes() / 2);
-        r(min, 6 * d.getMinutes());
-        r(sec, 6 * d.getSeconds());
-    }
+    let clockVal;
+    let eventCopy;
 
     if(beforeEvent) {
-        let clockVal = 'eventStartDate';
+        clockVal = eventStartDate;
+        eventCopy = `The next #crappyhour is June 22nd, 6-8pm at <a href="">One Mile House</a>.`;
     } else if(duringEvent) {
-        let clockVal = 'eventEndDate';
+        clockVal = eventEndDate;
+        eventCopy = `#crappyhour is happening right now until 8pm, at ` +
+        `<a href="">One Mile House</a>! If you hurry, you might be ` +
+        `able to make it. Sign up below for a reminder about next month's event.`;
     } else {
-        let clockVal = '88:88';
+        clockVal = '88:88';
+        eventCopy = `The next #crappyhour <a href="#">can't come soon enough</a>.`;
     }
 
     setClockNumbers(clockVal);
-
+    setClockHands(clockVal);
+    setCopy(eventCopy);
 }, 1000);
-
-// Content for before Event
-if(beforeEvent) {
-    $('.event-copy').append(
-        `The next #crappyhour is June 22nd, 6-8pm at <a href="">One Mile House</a>.`
-    );
-}
-
-// Content while event is happening
-if(duringEvent) {
-    $('.event-copy').append(
-        `#crappyhour is happening right now until 8pm, at <a href="">One Mile House</a>! If you
-        hurry, you might be able to make it. Sign up below for a reminder about next month's
-        event.`
-    );
-}
-
-// Content when the event is over
-if(afterEvent) {
-    $('.event-copy').append(
-        `The next #crappyhour <a href="#">can't come soon enough</a>.`
-    );
-}
